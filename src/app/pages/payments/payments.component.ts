0bd3f0b362af542c8322cbe1d9de20bc4031fcdc
Mateo Escobar <mateoescobar@outlook.es>
2025-07-03 10:41:51 -0500

    feat: se agrega el observable de los botones de funciones especiales fn

--- src/app/pages/payments/payments.component.ts
+++ src/app/pages/payments/payments.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { MaterialModule } from '../../material.module';
 import { ActionToolbarComponent } from "../../shared/action-toolbar/action-toolbar.component";
-
+import { FormsModule } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 export interface Payment {
-  id: number;
+  id?: number;
   date: string;
   amount: number;
   description?: string;
@@ -15,28 +16,70 @@ export interface Payment {
   templateUrl: './payments.component.html',
   imports: [
     MaterialModule,
-    ActionToolbarComponent
+    ActionToolbarComponent,
+    FormsModule
   ]
 })
 export class PaymentsComponent implements OnInit {
   payments: Payment[] = [];
+  //actions
+  formModel = {
+    date: '',
+    amount: 0,
+    description: ''
+  }
   selectedPayment: Payment | null = null;
 
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService, private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
 
   }
 
-  onCreate(){ console.log("Create clicked")}
   onEdit(){ console.log("Edit clicked")}
   onDelete(){ console.log("Dlete clicked")}
-  onSave(){ console.log("Save clicked")}
   onSearch(){ console.log("Search clicked")}
   onList(){
     this.api.getAll<Payment>('payments').subscribe({
       next: data => this.payments = data,
       error: err => console.error(err)
     });
+  }
+
+
+
+  onRowClick(event: any){
+    //event.data.date = event.data.date?.substring(0, 10);
+    this.selectedPayment = event.data;
+    this.formModel = { ...event.data };
+  }
+
+  onCreate(){
+    this.selectedPayment = null;
+    this.formModel = {
+      date: '',
+      amount: 0,
+      description: ''
+    }
+  }
+
+  onSave(){
+    if(this.selectedPayment){
+      this.api.update<Payment>('payments', this.selectedPayment?.id ?? 0, this.formModel).subscribe(() => {
+        this.onList();
+        this.snackBar.open('Payment updated successfully!', 'close', {
+          duration: 3000,
+          panelClass: 'snackbar-success'
+        });
+      });
+    }else{
+      this.api.create<Payment>('payments', this.formModel).subscribe(() => {
+        this.onList();
+        this.snackBar.open('Payment created successfully!', 'close', {
+          duration: 3000,
+          panelClass: 'snackbar-success'
+        });
+      });
+    }
   }
 }
--- src/app/shared/action-toolbar/action-toolbar.component.ts
+++ src/app/shared/action-toolbar/action-toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, HostListener } from '@angular/core';
 import { MaterialModule } from '../../material.module';
 
 @Component({
@@ -21,4 +21,22 @@ export class ActionToolbarComponent {
   @Output() delete = new EventEmitter<void>();
   @Output() search = new EventEmitter<void>();
   @Output() list = new EventEmitter<void>();
+
+  // Escuchar teclas globales
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    switch (event.key) {
+      case 'F2':
+        this.create.emit();
+        break;
+      case 'F9':
+        event.preventDefault();
+        this.list.emit();
+        break;
+      case 'F5':
+        event.preventDefault();
+        this.save.emit();
+        break;
+    }
+  }
 }
